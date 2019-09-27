d1dfa522eca4cfda4e28dedb8b1bf94e2b3a6e45
Kyujin Cho <kyujin.cho@lablup.com>
2019-09-27 10:07:11 +0900

    Prepare for upgrade to official API

--- meeco_app/lib/functions.dart
+++ meeco_app/lib/functions.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
@@ -5,44 +6,86 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:cookie_jar/cookie_jar.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:html/parser.dart' show parse;
 import 'package:meeco_app/types.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:yaml/yaml.dart';
+import 'package:oauth2/oauth2.dart' as oauth2;
 
 import 'exceptions.dart';
 
-const PROXY_ENABLED = false;
-const PROXY_IP = '192.168.43.99';
+const PROXY_ENABLED = true;
+const PROXY_IP = '172.17.142.251';
+const PROXY_PORT = '9090';
+
+Future<Uri> waitForUrl(BuildContext ctx, Uri authUrl) {
+  var completer = new Completer();
+  var webView = WebView(
+    initialUrl: 'https://meeco.kr${authUrl.path}',
+    navigationDelegate: (navigationRequest) {
+      if (navigationRequest.url.startsWith('http://meeco.app/')) {
+        completer.complete(Uri.parse(navigationRequest.url));
+        return NavigationDecision.prevent;
+      }
+      return NavigationDecision.navigate;
+    },
+  );
+  var alertDialog = new PlatformAlertDialog(
+    title: Text('OAuth2 Authentication'),
+    content: webView
+  );
+  showPlatformDialog(context: ctx, builder: (ctx) => alertDialog);
+  
+  return completer.future;
+}
 
 class Fetcher {
-  static Future<Dio> prepareDio() async {
-    var appDocDir = await getApplicationDocumentsDirectory();
-    var dio = new Dio();
-    if (PROXY_ENABLED) {
-      String proxy = Platform.isAndroid ? '$PROXY_IP:8888' : 'localhost:8888';
+  oauth2.Client authClient;
+  Dio dio;
 
-      // Tap into the onHttpClientCreate callback
-      // to configure the proxy just as we did earlier.
-      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (client) { 
-        // Hook into the findProxy callback to set the client's proxy.
-        client.findProxy = (uri) => 'PROXY $proxy';
-        
-        // This is a workaround to allow Charles to receive
-        // SSL payloads when your app is running on Android.
-        client.badCertificateCallback = (X509Certificate cert, String host, int port) => Platform.isAndroid;
-      };
-    }
-    dio.interceptors.add(CookieManager(PersistCookieJar(dir: appDocDir.absolute.path + '/dioCookie')));
+  Fetcher() {
+    dio = new Dio();
     dio.options.headers = {
       "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0_1 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A402 Safari/604.1",
       "Referer": "https://meeco.kr/"
     };
-    return dio;
   }
 
-  static Future<List<NormalRowInfo>> fetchNormal(String boardId, String category, int pageNum) async {
+  static Future<oauth2.Client> authenticate(BuildContext ctx) async { 
+    var rawCredentials = await rootBundle.loadString('assets/credentials.yml');
+    var appInfo = loadYaml(rawCredentials);
+    
+    final authEndpoint = Uri.parse('https://meeco.kr/?module=devcenter&act=dispDevcenterAuthorize&client_id=${appInfo['client_id']}');
+    final tokenEndpoint = Uri.parse('https://meeco.kr/?module=devcenter&act=token');
+
+    final redirectUrl = Uri.parse('http://meeco.app/oauth2-redirect');
+    final appDocDir = await getApplicationDocumentsDirectory();
+    final credFile = new File('${appDocDir.absolute.path}/oauth2/creds.json');
+
+    if (await credFile.exists()) {
+      var creds = new oauth2.Credentials.fromJson(await credFile.readAsString());
+      return new oauth2.Client(creds, identifier: appInfo['client_id'], secret: appInfo['client_secret']);
+    }
+
+    var grant = new oauth2.AuthorizationCodeGrant(appInfo['client_id'], authEndpoint, tokenEndpoint);
+    var authResultUrl = await waitForUrl(ctx, grant.getAuthorizationUrl(redirectUrl));
+    return await grant.handleAuthorizationResponse(authResultUrl.queryParameters);
+  }
+
+  Dio prepareDio() {
+    var dio = new Dio();
+    dio.options.headers = {
+      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0_1 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A402 Safari/604.1",
+      "Referer": "https://meeco.kr/"
+    };
+  }
+
+  Future<List<NormalRowInfo>> fetchNormal(String boardId, String category, int pageNum) async {
     print("fetchNormal called");
-    var dio = await prepareDio();
+    var dio = this.dio;
     var url = 'https://meeco.kr/index.php?mid=$boardId&page=$pageNum';
     if (category.length > 0) url += '&category=$category';
 
@@ -102,11 +145,11 @@ class Fetcher {
     return articles;
   }
 
-  static Future<ArticleInfo> fetchArticle({
+  Future<ArticleInfo> fetchArticle({
     @required String boardId,
     @required String articleId
   }) async {
-    var dio = await prepareDio();
+    var dio = this.dio;
     var response = await dio.get('https://meeco.kr/$boardId/$articleId');
     var document = parse(response.data.toString());
 
@@ -122,13 +165,13 @@ class Fetcher {
     var articleWriterId = '';
     
     if (document.querySelector('div.atc_ex table tbody') != null) {
-      var row = document.querySelector('div.atc_ex table tbody tr');
-      informationName = row.getElementsByTagName('tr')[0].text;
+      var row = document.querySelector('div.atc_ex table tbody');
+      informationName = row.children[0].text;
 
       if (row.querySelector('td a') != null) {
         informationValue = row.querySelector('td a').attributes['href'];
       } else {
-        informationValue = row.getElementsByTagName('td')[0].text;
+        informationValue = row.children[1].text;
       }
     }
 
@@ -153,6 +196,7 @@ class Fetcher {
 
     List<ReplyInfo> replyContainer = [];
     for (var reply in replies) {
+      if (reply.querySelector('div.xe_content') == null) continue;
       for (var anchor in reply.querySelectorAll('div.xe_content a')) {
         if (!anchor.attributes['href'].startsWith('https://meeco.kr/index.php?mid=sticker&sticker_srl=')) continue;
 
@@ -168,7 +212,7 @@ class Fetcher {
         }
         if (ruleMap.containsKey('background-image')) {
           var stickerUrl = ruleMap['background-image'];
-          anchor.innerHtml = '<img src="http://${stickerUrl.substring(4, stickerUrl.length - 1)} style="width: 300px; height: 300px;">';
+          anchor.innerHtml = '<img src="${stickerUrl.substring(5, stickerUrl.length - 1)}" style="width: 300px; height: 300px;">';
         }
       }
       replyContainer.add(ReplyInfo(
@@ -184,7 +228,7 @@ class Fetcher {
     }
 
     var profileImageUrl = '';
-    if (document.querySelector("article img.pf_img") != null) {
+    if (document.querySelector("article > header.atc_hd > div.atc_info > span > img.pf_img") != null) {
       profileImageUrl = document.querySelector("article img.pf_img").attributes['src'];
     }
 
@@ -207,8 +251,8 @@ class Fetcher {
     );
   }
 
-  static Future<Map<String, String>> getImageUploadRequiredData(String boardId) async {
-    var dio = await prepareDio();
+  Future<Map<String, String>> getImageUploadRequiredData(String boardId) async {
+    var dio = this.dio;
     var response = await dio.get('https://meeco.kr/index.php?mid=$boardId&act=dispBoardWrite');
     var document = parse(response.data.toString());
     var regex = RegExp('xe_editor_sequence: ([0-9]+)');
@@ -223,8 +267,50 @@ class Fetcher {
     };
   }
 
+  Future<String> postArticle({
+    @required String boardId,
+    @required String title,
+    @required String html,
+    @required String csrf,
+    String targetSrl,
+    String categorySrl
+  }) async {
+    var dio = this.dio;
+    var body = {
+      "_filter": "insert",
+      "error_return_url": "/index.php?mid=$boardId&act=dispBoardWrite",
+      "act": "procBoardInsertDocument",
+      "mid": boardId,
+      "content": html,
+      "title": title,
+      "comment_status": "ALLOW",
+      "_rx_csrf_token": csrf,
+      "use_editor": "Y",
+      "use_html": "Y",
+      "module": "board",
+      "_rx_ajax_compat": "XMLRPC",
+      "vid": "",
+      "document_srl": targetSrl != null ? targetSrl : '0',
+      "_saved_doc_message": "자동 저장된 글이 있습니다. 복구하시겠습니까? 글을 다 쓰신 후 저장하면 자동 저장 본은 사라집니다."
+    };
+    if (categorySrl != null && categorySrl.length > 0) {
+      body["category_srl"] = categorySrl;
+    }
+    var headers = {
+      'X-CSRF-Token': csrf,
+      'X-Requested-With': 'XMLHttpRequest'
+    };
 
-  static Future<Map<String, String>> uploadIamge({
+    var response = await dio.post('https://meeco.kr/', data: body, options: Options(
+      headers: headers,
+      responseType: ResponseType.json
+    ));
+
+    if (response.data['error'] != 0) throw GenericException('Server returned error: ${response.data['message']}');
+    return response.data['document_srl'];
+  }
+
+  Future<Map<String, String>> uploadIamge({
     String fileName, 
     String imageDataAsBase64,
     String boardId,
@@ -232,7 +318,7 @@ class Fetcher {
     String editorSequence,
     String csrfToken
   }) async {
-    var dio = await prepareDio();
+    var dio = this.dio;
     var nonce = 'T${new DateTime.now().millisecondsSinceEpoch}.${Random().nextDouble()}';
     var image = base64.decode(imageDataAsBase64);
 
@@ -266,49 +352,24 @@ class Fetcher {
     };
   }
 
-  static Future<String> tryLogin({
+  Future<String> tryLogin({
     @required String username,
     @required String password,
     bool persist = true
   }) async {
-    var dio = persist ? await prepareDio() : new Dio();
-    if (!persist) {
-      var cookieJar = DefaultCookieJar();
-      cookieJar.deleteAll();
-      dio.interceptors.add(CookieManager(cookieJar));
-
-      if (PROXY_ENABLED) {
-        String proxy = Platform.isAndroid ? '$PROXY_IP:8888' : 'localhost:8888';
-
-        // Tap into the onHttpClientCreate callback
-        // to configure the proxy just as we did earlier.
-        (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (client) { 
-          // Hook into the findProxy callback to set the client's proxy.
-          client.findProxy = (uri) => 'PROXY $proxy';
-          
-          // This is a workaround to allow Charles to receive
-          // SSL payloads when your app is running on Android.
-          client.badCertificateCallback = (X509Certificate cert, String host, int port) => Platform.isAndroid;
-        };
-      }
-
-      dio.options.headers = {
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0_1 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A402 Safari/604.1",
-        "Referer": "https://meeco.kr/"
-      };
-    }
+    var dio = this.dio;
     var response = await dio.get('https://meeco.kr/index.php?mid=index&act=dispMemberLoginForm');
 
     var document = parse(response.data.toString());
-    var csrfToken = document.querySelector('meta[name="csrf-token"]').attributes['content'];
     
     var ffForm = document.querySelector('form.ff');
     if (ffForm == null) {
-    response = await dio.get('https://meeco.kr');
-    document = parse(response.data.toString());
-    var exp = new RegExp(r'member_srl=([0-9]+)');
-    return exp.firstMatch(document.querySelector('div.mb_area.logged > a').attributes['href']).group(1);
+      response = await dio.get('https://meeco.kr');
+      document = parse(response.data.toString());
+      var exp = new RegExp(r'member_srl=([0-9]+)');
+      return exp.firstMatch(document.querySelector('div.mb_area.logged > a').attributes['href']).group(1);
     }
+    var csrfToken = document.querySelector('meta[name="csrf-token"]').attributes['content'];
     var validator = ffForm.querySelector('input[name="xe_validator_id"]');
 
     var body = {
@@ -343,7 +404,7 @@ class Fetcher {
     return exp.firstMatch(document.querySelector('div.mb_area.logged > a').attributes['href']).group(1);
   }
 
-  static String getStyledHTMLForArticle({String rawHtml, String color}) {
+  String getStyledHTMLForArticle({String rawHtml, String color}) {
     return '''<!DOCTYPE html>
                 <html lang="en">
                 <head>
@@ -366,7 +427,7 @@ class Fetcher {
     ''';
   }
 
-  static Color hexToColor(String code) {
+  Color hexToColor(String code) {
     return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
   }
 }
